eb0b97195e4bc65b08bfbe3f47f982f108ae18bd
LankayapalleHarika <harikal1003@gmail.com>
2017-11-30 11:55:13 +0530

    myFirst commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
+  imports: [ BrowserModule ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HighlightDirective
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
